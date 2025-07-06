4449da6ea67b0fdc3145a409a9e54747dab80088
Puneet-TH <22BCS13944@cuchd.in>
2025-07-06 20:54:04 +0530

    resolved error of authLogin

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -18,7 +18,7 @@ function Signup() {
           const userData = await authService.createAccount(data)
           if(userData){
             const userData = await authService.getCurrentUser()
-            if(userData) dispatch(authLogin({ userData }))
+            if(userData) dispatch(login({ userData }))
            navigate("/")
         }
         } catch (error) {
